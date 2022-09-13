a85217ccc84d8ab9abb7c0a09f1875bb5f8bca9b
Guille Fernandez <guille.fer79@hotmail.com>
2022-09-13 18:40:42 -0300

    preparamos menu Ecomerce para entrega de desafio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 /* import logo from './ejemplo.svg';  ACA IMPORTAMOS EL LOGO*/
-import './App.css';
+import "./App.css";
+import './components/NavBar/NavBar';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
 
-/*  */
+  /*  */
 
   return (
-    <div className="App">
+    <div className="NavBar">
       <header className="App-header">
-        
+        <h1>Tienda Online</h1>
       </header>
+      <NavBar />
+
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+function NavBar() {
+
+    /*  */
+    return (
+
+        <header className="App-header">
+            <nav>
+                <h2>DataShoop</h2>
+                <ul className="menu-principal">
+                    <li><a href="-"> Inicio </a></li>
+                    <li><a href="-">Nosotros</a></li>
+                    <li><a href="-">Categorias</a>
+                        <ul>
+                            <li><a href="-">Contacto</a></li>
+                            <li><a href="-">Envios</a></li>
+                            <li><a href="-">Reclamos</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="-">Tienda</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    );
+}
+
+export default NavBar;
