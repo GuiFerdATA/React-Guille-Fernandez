4021e95a7ff9adaa68c6a0108aa5a36afd1aac04
Guille Fernandez <guille.fer79@hotmail.com>
2022-09-16 02:16:23 -0300

    agregamos algunos cambios esteticos

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import "../NavBar/navbar.css"
 //2 - Importar componente FontAwsomeIcon
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // 3 - Importar icono deseado de alguna de la libreria de iconos gratuitos
 import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
 
+
 function CartWidget() {
     return (
-        <div>
+        <div className='icon-cart'>
             <FontAwesomeIcon icon={faCartArrowDown} />
             <span>0</span>
         </div>
