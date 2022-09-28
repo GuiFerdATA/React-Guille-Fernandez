98973227c355800b8c221756fe2aa84457dd23d7
Guille Fernandez <guille.fer79@hotmail.com>
2022-09-28 02:25:20 -0300

    dejamos comentados parte del desafio para seguir luego

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 import NavBar from '../src/components/NavBar/NavBar';
 /*import ItemListContainer from '../src/components/Products/ItemListContainer'*/
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+/*import { BrowserRouter, Routes, Route } from "react-router-dom";*/
 
 
 
 function App() {
   return (
+    /*<BrowserRouter>*/
     <div className="App">
-      
-
-        <NavBar />
-      
+      <NavBar />
+      {/*<Routes>*/}
       {/* <ItemListContainer greeting="¡Tienda online dataShop!" />*/}
       <ItemDetailContainer />
+      {/*</Routes>*/}
     </div>
-  );
+    /*</BrowserRouter>*/
+    );
 }
 
 export default App;
