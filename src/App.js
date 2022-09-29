a486cb6b1328eac3bc86e035ae65e90470748868
Guille Fernandez <guille.fer79@hotmail.com>
2022-09-29 14:14:02 -0300

    routing de ItemListContainer a / e ItemDetailContainer a /item/:id

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 /* import Logo from './media/Logo.png'; */
 import "./App.css";
 import NavBar from '../src/components/NavBar/NavBar';
-/*import ItemListContainer from '../src/components/Products/ItemListContainer'*/
+import ItemListContainer from '../src/components/Products/ItemListContainer';
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
-/*import { BrowserRouter, Routes, Route } from "react-router-dom";*/
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 
 function App() {
   return (
-    /*<BrowserRouter>*/
-    <div className="App">
-      <NavBar />
-      {/*<Routes>*/}
-      {/* <ItemListContainer greeting="¡Tienda online dataShop!" />*/}
-      <ItemDetailContainer />
-      {/*</Routes>*/}
-    </div>
-    /*</BrowserRouter>*/
-    );
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer greeting="¡Tienda online dataShop!" />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="*" element={<h1>404:ERROR</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
