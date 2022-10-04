405299873bd8f1f67d9b163e3a15d694f9dfaf48
Guille Fernandez <guille.fer79@hotmail.com>
2022-10-04 20:54:34 -0300

    preparamos proyecto cumpliendo normas del desafio, aplicamos stylo, etc

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -2,17 +2,28 @@ import React, { useState } from "react";
 import Button from "../Button/Button";
 import "./itemcount.css";
 
-function ItemCount({ stock, onAddToCart }) {
+function ItemCount({ stock, initial, onAddToCart }) {
     const [count, setCount] = useState(1);
 
     function handleAdd() {
+        if(count === stock){
+            alert("Máximo de Stock disponible!")
+    } else {
+        setCount(count + 1)
         if (count < stock) setCount(count + 1);
     }
+    initial = stock - count;
+}
 
     function handleSubstract() {
+        if(count === 1) {
+            alert("Necesitas tener un objeto de compra en tu carrito");
+    }else{
+        setCount(count - 1)
         console.log("ok");
         if (count > 1) setCount(count - 1);
     }
+}
 
     return (
         <div className="itemcount_container">
--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import "./itemdatailcontainer.css"
+import React, {useState} from 'react';
+import "./itemdatailcontainer.css";
+import {Link} from "react-router-dom";
 import ItemCount from '../ItemCount/ItemCount';
 
 
 
 function ItemDetail(props) {
 
+
+
+const [showItemsCount, setShowItemsCount] = useState(true);
+
 function onAddToCart(count){
     alert(`Agregaste ${count} productos a tu carrito de compras! `);
+    setShowItemsCount(false);
+    
 }
     return (
         <div className='cCs'>
@@ -17,9 +24,8 @@ function onAddToCart(count){
             </div>
             <div className="card-detail">
                 <p>{props.description}</p>
-                <h4>$ {props.price} 
-                <ItemCount initial={1} stock={6}  onAddToCart={onAddToCart}/>
-                </h4>
+                <h4>$ {props.price}</h4>
+                {showItemsCount === true? <ItemCount initial={1} stock={6}  onAddToCart={onAddToCart}/>: <Link to="/cart"> <button>Finalizar compra</button></Link>}
                 
             </div>
         </div>
